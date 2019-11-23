70feed58aa74758c24670c6df3543c0153943fa5
wqs <1243411677@qq.com>
2019-11-23 14:27:19 +0800

    github依赖测试

---
+++ AsSdkDemo/app/src/main/java/com/example/assdkdemo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.assdkdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.mylibrary.LibraryActivity;
+
+//后来写的
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LibraryActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ AsSdkDemo/mylibrary/src/main/java/com/example/mylibrary/LibraryActivity.java
@@ -0,0 +1,19 @@
+package com.example.mylibrary;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * wuqingsen on 2019-11-23
+ * Mailbox:1243411677@qq.com
+ * annotation:
+ */
+public class LibraryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_library);
+    }
+}
